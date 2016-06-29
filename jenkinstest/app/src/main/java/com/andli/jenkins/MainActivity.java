2c69e8029628c43b256234a58b1a5e5e1f00f374
李林 <ww1095@163.com>
2016-06-29 11:30:09 +0800

    自定义生成的APK包名

--- jenkinstest/app/src/main/java/com/andli/jenkins/MainActivity.java
+++ jenkinstest/app/src/main/java/com/andli/jenkins/MainActivity.java
@@ -3,6 +3,12 @@ package com.andli.jenkins;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ *author andli
+ *create at 16/6/27 下午9:01
+ *
+**/
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
